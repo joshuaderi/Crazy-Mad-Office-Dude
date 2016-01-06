3622fa4d55b5ea50d69b53c1bc46eccb7c6907e6
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-06 10:07:28 +0200

    Registering As a Listener

---
+++ Assets/Scripts/NotificationsManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NotificationsManager : MonoBehaviour {
+
+	private Dictionary<string, List<Component>> Listeners = new Dictionary<string, List<Component>>();
+    //Function to add a listener for an notification to the listeners list
+    public void AddListener(Component Listener, string NotificationName)
+    {
+        //Add listener to dictionary
+        if (!Listeners.ContainsKey(NotificationName))
+        {
+            Listeners.Add(NotificationName, new List<Component>());
+        }
+
+        //Add object to listener list for this notification
+        Listeners[NotificationName].Add(Listener);
+    }
+}
