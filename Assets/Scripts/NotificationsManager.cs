e37ad89969c1a9f82d427abd45fc18b1beb82f3b
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-06 10:24:41 +0200

    posting notifications

--- Assets/Scripts/NotificationsManager.cs
+++ Assets/Scripts/NotificationsManager.cs
@@ -2,19 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class NotificationsManager : MonoBehaviour {
+public class NotificationsManager : MonoBehaviour
+{
 
-	private Dictionary<string, List<Component>> Listeners = new Dictionary<string, List<Component>>();
+    private Dictionary<string, List<Component>> listeners = new Dictionary<string, List<Component>>();
     //Function to add a listener for an notification to the listeners list
-    public void AddListener(Component Listener, string NotificationName)
+    public void AddListener(Component listener, string notificationName)
     {
         //Add listener to dictionary
-        if (!Listeners.ContainsKey(NotificationName))
+        if (!listeners.ContainsKey(notificationName))
         {
-            Listeners.Add(NotificationName, new List<Component>());
+            listeners.Add(notificationName, new List<Component>());
         }
 
         //Add object to listener list for this notification
-        Listeners[NotificationName].Add(Listener);
+        listeners[notificationName].Add(listener);
+    }
+
+    //Function to post a notification to a listener
+    public void PostNotification(Component sender, string notificationName)
+    {
+        //If not key in dictionary exists, then exit
+        if (!listeners.ContainsKey(notificationName)) return;
+
+        //Else post notification to all matching listeners
+        foreach (Component listener in listeners[notificationName])
+        {
+            listener.SendMessage(notificationName, sender, SendMessageOptions.DontRequireReceiver);
+        }
     }
 }
