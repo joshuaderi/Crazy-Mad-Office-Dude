9a6646d55e2a0d1e2a93d74aebcea5b4cb67520b
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-06 16:56:18 +0200

    add Billboard script

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+    private Transform thisTransform = null;
+
+    void Start()
+    {
+        //Cache transform
+        thisTransform = transform;
+    }
+
+    void LateUpdate()
+    {
+        //Billboard sprite
+        Vector3 cameraPos = Camera.main.transform.position;
+        
+        float deltaX = thisTransform.position.x - cameraPos.x;
+        float deltaZ = thisTransform.position.z - cameraPos.z;
+
+        Vector3 lookAtDir = new Vector3(deltaX, 0, deltaZ).normalized;
+        thisTransform.rotation = Quaternion.LookRotation(lookAtDir, Vector3.up);
+    }
+}
