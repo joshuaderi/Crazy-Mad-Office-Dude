853a6ec991c3c3294b70b73ea96cce807671402a
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-07 20:30:29 +0200

    add GameManager and PowerupDollar
    add collider to powerup, tag player and audioSource

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿//GameManager
+//Singleton and persistent object to manage game state
+//For high level control over game
+
+using UnityEngine;
+
+//Game Manager requires other manager components
+[RequireComponent(typeof(NotificationsManager))] //Component for sending and receiving notifications
+
+public class GameManager : MonoBehaviour
+{
+    //C# property to retrieve currently active instance of object, if any
+    public static GameManager Instance
+    {
+        //create game manager object if required
+        get { return instance ?? (instance = new GameObject("GameManager").AddComponent<GameManager>()); }
+    }
+
+    //Internal reference to single active instance of object - for singleton behaviour
+    private static GameManager instance;
+
+    //C# property to retrieve notifications manager
+    public static NotificationsManager Notifications
+    {
+        get { return notifications ?? (notifications = instance.GetComponent<NotificationsManager>()); }
+    }
+
+    //Internal reference to notifications object
+    private static NotificationsManager notifications;
+
+    //Called before Start on object creation
+    void Awake()
+    {
+        //Check if there is an existing instance of this object
+        if ((instance == null) || (instance.GetInstanceID() == GetInstanceID()))
+        {
+            instance = this; //Make this object the only instance
+            DontDestroyOnLoad(gameObject); //Set as do not destroy
+        }
+        else DestroyImmediate(gameObject); //Delete duplicate
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //Add cash collected listener to listen for win condition
+        Notifications.AddListener(this, "CashCollected");
+    }
+
+    //Function called when all cash is collected in level
+    public void CashCollected(Component sender)
+    {
+    }
+
+    //Restart Game
+    public void RestartGame()
+    {
+        //Load first level
+        Application.LoadLevel(0);
+    }
+
+    //Exit Game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PowerupDollar.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PowerupDollar : MonoBehaviour
+{
+    //Amount of cash to give player
+    public float CashAmount = 100.0f;
+
+    //Audio Clip for this object
+    public AudioClip Clip = null;
+
+    //Audio Source for sound playback
+    private AudioSource sfx;
+
+    void Start()
+    {
+        //Find sound object in scene
+        GameObject soundsObject = GameObject.FindGameObjectWithTag("sounds");
+
+        //If no sound object, then exit
+        if (soundsObject == null) return;
+
+        //Get audio source component for sfx
+        sfx = soundsObject.GetComponent<AudioSource>();
+    }
+
+    //Event triggered when colliding with player
+    void OnTriggerEnter(Collider other)
+    {
+        //Is colliding object a player? Cannot collide with enemies
+        if (!other.CompareTag("player")) return;
+
+        //Play collection sound, if audio source is available
+        if (sfx != null) sfx.PlayOneShot(Clip, 1.0f);
+
+        //Hide object from level so it cannot be collected more than once
+        gameObject.SetActive(false);
+
+        //Get PlayerController object and update cash
+        //PlayerController PC = other.gameObject.GetComponent<PlayerController>();
+
+        //If there is a PC attached to colliding object, then update cash
+        //if(PC) PC.Cash += CashAmount;
+
+        //Post power up collected notification, so other objects can handle this event if required
+        GameManager.Notifications.PostNotification(this, "PowerupCollected");
+    }
+}
