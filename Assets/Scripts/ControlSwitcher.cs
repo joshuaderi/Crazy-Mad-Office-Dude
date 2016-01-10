b967c379a7c13a4146eed63c6f8745f80e9b1c33
Pavel Kvas <kvaspasha@yandex.ru>
2016-01-10 15:13:42 +0200

    refactor ControlSwitcher

--- Assets/Scripts/ControlSwitcher.cs
+++ Assets/Scripts/ControlSwitcher.cs
@@ -1,23 +1,21 @@
-﻿//------------------------------------------------
-using UnityEngine;
-
-//------------------------------------------------
+﻿using UnityEngine;
 public class ControlSwitcher : MonoBehaviour
 {
-	//------------------------------------------------
-	//Reference to desktop first person controller (default)
-	public GameObject DesktopFirstPerson = null;
-	
-	//Reference to mobile first person controller
-	public GameObject MobileFirstPerson = null;
-	 
-	//Select appropriate first person control for platform
-	void Awake()
-	{
-		//If mobile platform, then use mobile first person controller
-		#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8
-		DesktopFirstPerson.SetActive(false);
-		MobileFirstPerson.SetActive(true);
-		#endif
-	}
+    //Reference to desktop first person controller (default)
+    public GameObject DesktopFirstPerson = null;
+
+    //Reference to mobile first person controller
+    public GameObject MobileFirstPerson = null;
+
+    //Select appropriate first person control for platform
+    void Awake()
+    {
+        bool isMobile = false;
+        //If mobile platform, then use mobile first person controller
+#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8
+        isMobile = true;
+#endif
+        DesktopFirstPerson.SetActive(!isMobile);
+        MobileFirstPerson.SetActive(isMobile);
+    }
 }
